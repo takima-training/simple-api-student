cc2cca0e13063b457ae80615d485865027e38d80
Maxime <mpakula@takima.fr>
2025-01-29 14:25:28 +0100

    Upgrade to java-21

--- src/main/java/fr/takima/training/simpleapi/entity/Department.java
+++ src/main/java/fr/takima/training/simpleapi/entity/Department.java
@@ -1,15 +1,22 @@
 package fr.takima.training.simpleapi.entity;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 import java.util.List;
 
 @Entity
 @Table(name = "departments")
 public class Department {
     @Id
-    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotNull
